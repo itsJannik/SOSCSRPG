8055550dee7108eeccbddef8c42dea0889ea62c2
Jannik <jannik.lorenz.mail@web.de>
2020-11-02 23:08:36 +0100

    lesson0304 added ViewModel Layer (Controll Layer)

---
+++ SOSCSRPG/Engine/ViewModels/GameSession.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    class GameSession
+    {
+        Player CurrentPlayer { get; set; }
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Jannik";
+            CurrentPlayer.Gold = 1_000_000;
+        }
+    }
+}
