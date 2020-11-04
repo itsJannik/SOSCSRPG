c51592c99b910d34243d745f29f79220870a54ea
Jannik <jannik.lorenz.mail@web.de>
2020-11-04 19:49:03 +0100

    lesson0401 added images but the inclusion didnt work, started trying to use resources

--- SOSCSRPG/Engine/ViewModels/GameSession.cs
+++ SOSCSRPG/Engine/ViewModels/GameSession.cs
@@ -8,6 +8,7 @@ namespace Engine.ViewModels
     public class GameSession
     {
         public Player CurrentPlayer { get; set; }
+        public Location CurrentLocation { get; set; }
         public GameSession()
         {
             CurrentPlayer = new Player();
@@ -17,6 +18,16 @@ namespace Engine.ViewModels
             CurrentPlayer.HitPoints = 10;
             CurrentPlayer.ExperiencePoints = 0;
             CurrentPlayer.Level = 1;
+
+            CurrentLocation = new Location();
+            CurrentLocation.Name = "Home";
+            CurrentLocation.XCoordinate = 0;
+            CurrentLocation.YCoordinate = -1;
+            CurrentLocation.Description = "This is your house";
+            //CurrentLocation.ImageName = "Engine/Images/Locations/SpiderForest.png";
+            CurrentLocation.ImageName = "/Engine;component/Images/Locations/SpiderForest.png";
+            // the following does work but is ugly
+            //CurrentLocation.ImageName = "C:/Users/nanni/source/repos/SOSCSRPG/Engine/Images/Locations/SpiderForest.png";
         }
     }
 }
